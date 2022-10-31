15b184711fe6cba4c03dcdd37d5f4fe6befc4399
Nikula Daniil <daniil.nikula@gmail.com>
2022-10-31 12:20:26 +0200

    Добавьте файлы проекта.

---
+++ Module 9.6 FinalTask 1/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Module_9._6_FinalTask_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Exception[] exception = new Exception[5];
+            exception[0] = new FormatException("Format Exception");
+            exception[1] = new DivideByZeroException("Divide by zero");
+            exception[2] = new ArgumentException("Argument Exception");
+            exception[3] = new DirectoryNotFoundException("Directory Not Found");
+            exception[4] = new MyException("Exception text");
+            try
+            {
+                throw exception[4];
+
+            }
+            catch(MyException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+}
